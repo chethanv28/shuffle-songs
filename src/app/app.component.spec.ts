142e54f933c9971ec5bad4e376cda5b5399fe315
Chethan Venkatesh <chethan.venkatesh@veritas.com>
2019-03-04 17:07:48 -0800

    push to github

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to song-shuffler!');
+    expect(compiled.querySelector('h1').textContent).toContain('song-shuffler!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'song-shuffler';
+  items = ['Song 1','Song 2','Song 3','Song 4','Song 5','Song 6','Song 7','Song 8','Song 9','Song 10']
+
+  constructor() {}
+  ngOnInit() {}
+
+  public shuffle(event) {
+    var l = this.items.length, current, index;
+    // We use the back of the array to store the shuffled elements, and the front of the array to store the remaining elements
+    while (l) {
+
+      // pick a random remaining element (from the front) and place in its new location (in the back).
+      index = Math.floor(Math.random() * l--);
+
+      // And swap it with the current element.
+      current = this.items[l];
+      this.items[l] = this.items[index];
+      this.items[index] = current;
+    }
+  }
+  public ngOnDestroy() {}
 }
